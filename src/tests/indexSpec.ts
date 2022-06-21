a715a128b530cfdf81067a20c7a0ae965c0cb40f
Omar Abd el hamid <omarhenawy31@gmail.com>
2022-06-21 23:43:29 +0200

    handling prams2

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -6,11 +6,11 @@ import express from 'express';
 const request = supertest(app);
 
 describe('Test endpoint responses', () => {
-  it('gets the api endpoint', async () => {
+  it('gets the api endpoint', async ():Promise<void> => {
     const response = await request.get('/api');
     expect(response.status).toBe(200);
   });
-  it('gets the images endpoint', async () => {
+  it('gets the images endpoint', async ():Promise<void> => {
     const response  = await request.get(
       '/api/images?filename=fjord&height=200&width=200'
     );
